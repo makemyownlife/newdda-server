f3d9df66fdc56be7530223e0aeb0348523e3cb31
zhangyong <zhangyong7120180@163.com>
2014-11-27 19:42:28 +0800

    添加 mysql 包

--- src/main/java/com/elong/pb/newdda/net/mysql/HandshakePacket.java
+++ src/main/java/com/elong/pb/newdda/net/mysql/HandshakePacket.java
@@ -1,14 +1,17 @@
 package com.elong.pb.newdda.net.mysql;
 
+import com.elong.pb.newdda.common.BufferUtil;
+
 import java.nio.ByteBuffer;
 
 /**
  * Created by zhangyong on 14/11/23.
  * From server to client during initial handshake.
  * mysql jdbc 链接 mysql server 服务端发送给客户端 握手包
- *
  */
 public class HandshakePacket extends MysqlPacket {
+
+    private static final byte[] FILLER_13 = new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     public byte protocolVersion;
 
@@ -28,17 +31,42 @@ public class HandshakePacket extends MysqlPacket {
 
     @Override
     public ByteBuffer encode() {
-        return null;
+        ByteBuffer buffer = ByteBuffer.allocate(
+               3
+               + 1
+               + calcPacketSize()
+        );
+        BufferUtil.writeUB3(buffer, calcPacketSize());
+        buffer.put(packetId);
+        buffer.put(protocolVersion);
+        BufferUtil.writeWithNull(buffer, serverVersion);
+        BufferUtil.writeUB4(buffer, threadId);
+        BufferUtil.writeWithNull(buffer, seed);
+        BufferUtil.writeUB2(buffer, serverCapabilities);
+        buffer.put(serverCharsetIndex);
+        BufferUtil.writeUB2(buffer, serverStatus);
+        buffer.put(FILLER_13);
+        // buffer.position(buffer.position() + 13);
+        BufferUtil.writeWithNull(buffer, restOfScrambleBuff);
+        buffer.flip();
+        return buffer;
     }
 
     @Override
     public int calcPacketSize() {
-        return 0;
+        int size = 1;
+        size += serverVersion.length;// n
+        size += 5;// 1+4
+        size += seed.length;// 8
+        size += 19;// 1+2+1+2+13
+        size += restOfScrambleBuff.length;// 12
+        size += 1;// 1
+        return size;
     }
 
     @Override
     protected String getPacketInfo() {
-        return null;
+        return "MySQL Handshake Packet";
     }
 
 }
--- src/main/java/com/elong/pb/newdda/server/NettyFrontHandler.java
+++ src/main/java/com/elong/pb/newdda/server/NettyFrontHandler.java
@@ -2,6 +2,7 @@ package com.elong.pb.newdda.server;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.MessageToByteEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,13 +10,13 @@ import org.slf4j.LoggerFactory;
 /**
  * Created by zhangyong on 14/11/22.
  */
-public class NettyFrontHandler extends MessageToByteEncoder{
+public class NettyFrontHandler extends SimpleChannelInboundHandler {
 
     private Logger logger = LoggerFactory.getLogger(NettyFrontHandler.class);
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception {
-        logger.info("encoding");
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        logger.debug("handler msg==" + msg);
     }
 
 }
