36c0b499f6d610d9572a13e7ad47bd4edc928fcb
zhangyong <zhangyong7120180@163.com>
2015-03-14 12:58:05 +0800

    路由的结果集

---
+++ src/main/java/com/elong/pb/newdda/route/RouteResultSet.java
@@ -0,0 +1,14 @@
+package com.elong.pb.newdda.route;
+
+/**
+ * 路由的结果
+ * User: zhangyong
+ * Date: 2015/3/14
+ * Time: 12:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class RouteResultSet {
+
+
+
+}
