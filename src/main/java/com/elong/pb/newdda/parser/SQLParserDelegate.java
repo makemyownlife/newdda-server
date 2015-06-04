9217b40291de66db925a569b5ec074cc1c2fdfce
zhangyong <zhangyong7120180@163.com>
2015-06-04 22:35:32 +0800

    解析 sql  成visitor

--- src/main/java/com/elong/pb/newdda/parser/SQLParserDelegate.java
+++ src/main/java/com/elong/pb/newdda/parser/SQLParserDelegate.java
@@ -36,12 +36,52 @@ public final class SQLParserDelegate {
     }
 
     public static SQLStatement parse(String sql, MySQLLexer lexer, String charset) throws SQLSyntaxErrorException {
-        return null;
+        try {
+            SQLStatement stmt = null;
+            boolean isEOF = true;
+            stmtSwith:
+            switch (lexer.token()) {
+                case KW_SELECT:
+                case PUNC_LEFT_PAREN:
+                    stmt = null;
+                    break stmtSwith;
+                default:
+                    throw new SQLSyntaxErrorException("sql is not a supported statement");
+            }
+            if (isEOF) {
+                while (lexer.token() == MySQLToken.PUNC_SEMICOLON) {
+                    lexer.nextToken();
+                }
+                if (lexer.token() != MySQLToken.EOF) {
+                    throw new SQLSyntaxErrorException("SQL syntax error!");
+                }
+            }
+            return stmt;
+        } catch (Exception e) {
+            throw new SQLSyntaxErrorException(buildErrorMsg(e, lexer, sql), e);
+        }
+    }
+
+    private static String buildErrorMsg(Exception e, MySQLLexer lexer, String sql) {
+        StringBuilder sb = new StringBuilder(
+                "You have an error in your SQL syntax; Error occurs around this fragment: ");
+        final int ch = lexer.getCurrentIndex();
+        int from = ch - 16;
+        if (from < 0) {
+            from = 0;
+        }
+        int to = ch + 9;
+        if (to >= sql.length()) {
+            to = sql.length() - 1;
+        }
+        String fragment = sql.substring(from, to + 1);
+        sb.append('{').append(fragment).append('}').append(". Error cause: " + e.getMessage());
+        return sb.toString();
     }
 
     public static void main(String[] args) throws SQLSyntaxErrorException {
-        MySQLLexer lexer = new MySQLLexer("select * from users where a = 1");
-
+        MySQLLexer lexer = new MySQLLexer("  select * from users where a = 1");
+        System.out.println(111);
     }
 
 }
---
+++ src/test/java/com/elong/pb/newdda/route/DdaRouteTest.java
@@ -0,0 +1,39 @@
+package com.elong.pb.newdda.route;
+
+import com.alibaba.cobar.parser.ast.stmt.SQLStatement;
+import com.alibaba.cobar.parser.recognizer.SQLParserDelegate;
+import com.alibaba.cobar.parser.recognizer.mysql.syntax.MySQLParser;
+import com.elong.pb.newdda.config.DataSourceConfig;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by zhangyong on 15/5/31.
+ * dda 相关的路由 测试
+ */
+public class DdaRouteTest {
+
+    private static final String charset = "UTF-8";
+
+    @Test
+    public void routeTest() throws Exception {
+        String stmt = "select * from user a  where a.card_no = '420116198401082811' ";
+        SQLStatement ast = SQLParserDelegate.parse(stmt, charset == null ? MySQLParser.DEFAULT_CHARSET : charset);
+
+        DataSourceConfig config = new DataSourceConfig();
+        config.setKeyname("card_no");
+        PartitionKeyVisitor visitor = new PartitionKeyVisitor(config);
+        ast.accept(visitor);
+
+        //判断列的值
+        Map<String, Map<String, List<Object>>> astExt = visitor.getColumnValue();
+        for (Map.Entry<String, Map<String, List<Object>>> e : astExt.entrySet()) {
+            Map<String, List<Object>> col2Val = e.getValue();
+            System.out.println(col2Val);
+        }
+
+    }
+
+}
