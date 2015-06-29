adb3f56cce3bcaa0fd42696c3b6b212f26369efc
zhangyong <zhangyong7120180@163.com>
2015-06-29 17:50:08 +0800

    添加  分布式中间件的main的部分 。

--- src/main/java/com/elong/pb/newdda/main/DdaMain.java
+++ src/main/java/com/elong/pb/newdda/main/DdaMain.java
@@ -12,7 +12,9 @@ public class DdaMain {
     private static Logger logger = LoggerFactory.getLogger(DdaMain.class);
 
     public static void main(String[] args) {
-        
+        long start = System.currentTimeMillis();
+        logger.info("开始启动分布式数据库中间件dda ");
+        logger.info("结束启动分布式数据库中间件dda 总耗时:{}毫秒", System.currentTimeMillis() - start);
     }
 
 }
