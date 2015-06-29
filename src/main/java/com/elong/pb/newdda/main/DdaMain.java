625a1e621d81e98dafccb6870c1f2449a2132598
zhangyong <zhangyong7120180@163.com>
2015-06-29 17:43:44 +0800

    DdaMain 我们这一版的目的是为了可以部署一个新版 可以在生产上使用的

--- src/main/java/com/elong/pb/newdda/main/DdaMain.java
+++ src/main/java/com/elong/pb/newdda/main/DdaMain.java
@@ -1,10 +1,15 @@
-package com.elong.pb.newdda;
+package com.elong.pb.newdda.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * DDA 分布式数据库中间件的
  * Created by zhangyong on 15/6/29.
  */
 public class DdaMain {
+
+    private static Logger logger = LoggerFactory.getLogger(DdaMain.class);
 
     public static void main(String[] args) {
         
