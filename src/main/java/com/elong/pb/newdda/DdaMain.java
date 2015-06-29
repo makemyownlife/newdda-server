888bf2b1bb86d6b19192d130857c99f477584187
zhangyong <zhangyong7120180@163.com>
2015-06-29 17:32:30 +0800

    再次提交一版 应该是有问题的

--- src/main/java/com/elong/pb/newdda/DdaMain.java
+++ src/main/java/com/elong/pb/newdda/DdaMain.java
@@ -1,25 +1,13 @@
 package com.elong.pb.newdda;
 
-import com.elong.pb.newdda.server.DdaServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * Created by zhangyong on 14/11/21.
- * 张勇的新DDA的服务 用于关键字分库
+ * DDA 分布式数据库中间件的
+ * Created by zhangyong on 15/6/29.
  */
 public class DdaMain {
 
-    private static Logger logger = LoggerFactory.getLogger(DdaMain.class);
-
     public static void main(String[] args) {
-        try {
-            DdaServer server = DdaServer.getInstance();
-            server.start();
-        } catch (Throwable e) {
-            logger.error("main start error: ", e);
-            System.exit(-1);
-        }
+        
     }
 
 }
