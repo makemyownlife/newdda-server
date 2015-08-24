9c49aedb6cf31c26bacd71f751998a39c7a90cfa
zhangyong <zhangyong7120180@163.com>
2015-08-24 22:28:23 +0800

    说明 snowflake的算法

--- src/main/java/com/elong/pb/newdda/common/IdWorker.java
+++ src/main/java/com/elong/pb/newdda/common/IdWorker.java
@@ -11,10 +11,18 @@ import com.elong.pb.newdda.exception.InvalidSystemClock;
  * @author boyan
  * @Date 2011-4-27
  *
+ *  核心代码为其IdWorker这个类实现，其原理结构如下，我分别用一个0表示一位，用—分割开部分的作用：
+
+1
+0---0000000000 0000000000 0000000000 0000000000 0 --- 00000 ---00000 ---000000000000
+在上面的字符串中，第一位为未使用（实际上也可作为long的符号位），接下来的41位为毫秒级时间，然后5位datacenter标识位，5位机器ID（并不算标识符，实际是为线程标识），然后12位该毫秒内的当前毫秒内的计数，加起来刚好64位，为一个Long型。
+
+这样的好处是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞（由datacenter和机器ID作区分），并且效率较高，经测试，snowflake每秒能够产生26万ID左右，完全满足需要
  */
 public class IdWorker {
 
     private final long workerId;
+    //1361753741828L
     private final static long twepoch = 1303895660503L;
     private long sequence = 0L;
     private final static long workerIdBits = 10L;
