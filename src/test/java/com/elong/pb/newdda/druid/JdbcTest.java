c63a0885e0c24c413a90d52ecebdc848d88ee125
zhangyong <zhangyong7120180@163.com>
2016-02-03 17:28:12 +0800

    jdbc 修改 以及 部署配置

--- src/test/java/com/elong/pb/newdda/druid/JdbcTest.java
+++ src/test/java/com/elong/pb/newdda/druid/JdbcTest.java
@@ -1,6 +1,12 @@
 package com.elong.pb.newdda.druid;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * 测试jdbc
@@ -8,9 +14,29 @@ import org.junit.Test;
  */
 public class JdbcTest {
 
-    @Test
-    public void testJdbcConnect(){
+    @Before
+    public void setUp() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void testJdbcConnect() {
+        String url = "jdbc:mysql://10.100.19.144:3306/test";
+        String username = "root";
+        String password = "ilxw";
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
+    }
+
+    @After
+    public void destory() {
 
     }
 
