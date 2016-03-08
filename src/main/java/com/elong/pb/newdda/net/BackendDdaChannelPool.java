fec597af43f694b1bd1326cf8017a21ec30e2c91
zhangyong <zhangyong7120180@163.com>
2016-03-08 11:52:22 +0800

    后端连接池 的封装

---
+++ src/main/java/com/elong/pb/newdda/net/BackendDdaChannelPool.java
@@ -0,0 +1,20 @@
+package com.elong.pb.newdda.net;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 后端 每个location的池子
+ * User: zhangyong
+ * Date: 2016/3/8
+ * Time: 11:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class BackendDdaChannelPool {
+
+    private static final Logger logger = LoggerFactory.getLogger(BackendDdaChannelPool.class);
+
+
+
+}
+
